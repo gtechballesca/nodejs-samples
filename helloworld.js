18ead804b891fb2029b4c484c127241fd7c1638b
Gil Ballesca <gilballesca@gilballesca.local>
2025-03-27 06:59:53 +0800

    updates

--- helloworld.js
+++ helloworld.js
@@ -11,3 +11,12 @@ ch();
 
 const isURL = validator.isURL('https://www.google.com');
 console.log(`Is Google.com is a URL ${isURL}`); // true
+
+
+const cmd = process.argv[2];
+console.log(`Command is ${cmd}`); // node helloworld.js test
+if (cmd === 'helloworld') {
+    console.log(`We received your command ${cmd}`);
+} else {
+    console.log(`Unknown command ${cmd}`);
+}
